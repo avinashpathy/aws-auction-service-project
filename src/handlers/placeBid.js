1e3ec44e1eb7218ab44ad86270865ebcd03a5e40
Avinash Pathy <avinashpathy2001@gmail.com>
2023-01-08 11:58:12 +0530

    Specified seller and bidder identity

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -8,12 +8,25 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const {id} = event.pathParameters;
   const {amount} = JSON.parse(event.body);
+  const {email} = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
 
+  //Bid identity validation 
+  if(email === auction.seller){
+    throw new createError.Forbidden(`You cannot bid on your own auctions!`);
+  }
+
+  //Avoid double bidding
+  if(email === auction.highestBid.bidder){
+    throw new createError.Forbidden(`You are already the highest bidder!`);
+  }
+
+  // Auction status validation
   if(auction.status !== 'OPEN'){
     throw new createError.Forbidden(`You cannot bid on closed auctions!`);
   }
 
+  //Bid amount validation
   if(amount<= auction.highestBid.amount){
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
   }
@@ -21,9 +34,10 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key:{id},
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
         ':amount': amount,
+        ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   }
